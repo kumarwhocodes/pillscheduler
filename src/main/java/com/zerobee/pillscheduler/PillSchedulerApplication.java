d1d89d6bde6ee5cd28138213601739af786e87a2
Kumar Sambhav <sambhav26k@gmail.com>
2024-12-26 20:12:31 +0530

    feat - added MarkDoseAsTaken Endpoint and implemented cron jobs for resetting doses every midnight

--- src/main/java/com/zerobee/pillscheduler/PillSchedulerApplication.java
+++ src/main/java/com/zerobee/pillscheduler/PillSchedulerApplication.java
@@ -3,8 +3,10 @@ package com.zerobee.pillscheduler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 @ComponentScan(basePackages = "com.zerobee.pillscheduler.*")
 public class PillSchedulerApplication {
 
